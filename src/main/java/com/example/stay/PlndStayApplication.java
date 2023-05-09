3a0337cbbb689531c1aa3ed58f153a3a7d910a60
seokbeom <itseokbeom@gmail.com>
2023-05-09 11:03:31 +0900

    first commit

---
+++ src/main/java/com/example/stay/PlndStayApplication.java
@@ -0,0 +1,13 @@
+package com.example.stay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlndStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlndStayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stay/PlndStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlndStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
