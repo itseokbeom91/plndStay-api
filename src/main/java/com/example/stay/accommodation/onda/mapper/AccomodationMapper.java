f320feac8f0146aaaf3580d8c5097e7fec6bcfdf
yoojung <syjung618@naver.com>
2023-05-16 17:39:32 +0900

    onda roomType Insert

--- src/main/java/com/example/stay/accommodation/onda/mapper/AccomodationMapper.java
+++ src/main/java/com/example/stay/accommodation/onda/mapper/AccomodationMapper.java
@@ -3,17 +3,20 @@ package com.example.stay.accommodation.onda.mapper;
 import com.example.stay.openMarket.common.dto.CancelInfoDto;
 import com.example.stay.openMarket.common.dto.CondoDto;
 import com.example.stay.openMarket.common.dto.ContentsPhotoDto;
+import com.example.stay.openMarket.common.dto.ToconDto;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccomodationMapper {
-    CondoDto selectCondoByConId(String con_id);
+//    CondoDto selectCondoByConId(String con_id);
 
-    String selectCode(String con_flag);
+//    String selectCode(String con_flag);
 
     String accommRegist(CondoDto accommDetail);
 
     String accommPhotoContentsReg(ContentsPhotoDto contentsPhotoDto);
 
     String cancelInfoReg(CancelInfoDto cancelInfoDto);
+
+    String roomTypeRegist(ToconDto toconDto);
 }
