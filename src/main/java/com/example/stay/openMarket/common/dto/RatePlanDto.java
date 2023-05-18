8b452cb17d28311359147ba694b6cae9f9b2eb81
yoojung <syjung618@naver.com>
2023-05-18 09:14:25 +0900

    onda ratePlan Insert

--- src/main/java/com/example/stay/openMarket/common/dto/RatePlanDto.java
+++ src/main/java/com/example/stay/openMarket/common/dto/RatePlanDto.java
@@ -1,2 +1,23 @@
-package com.example.stay.openMarket.common.dto;public class RatePlanDto {
+package com.example.stay.openMarket.common.dto;
+
+import lombok.Data;
+
+@Data
+public class RatePlanDto {
+    private int intConId;
+    private String strAccommId;
+    private int intToconIdx;
+    private String strRoomTypeId;
+    private String strRatePlanId;
+    private String strRatePlanName;
+    private String bedTypeCode;
+    private String strMealCode;
+    private int intMinPersons;
+    private int intMaxPersons;
+    private int intPrice;
+    private String strDelYn;
+//    private int intMinStay;
+//    private int intMaxStay;
+
+
 }
