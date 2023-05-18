6e73fb992f76203e05cb2351739a02efd7445941
seokbeom <itseokbeom@gmail.com>
2023-05-18 17:58:46 +0900

    프로시저 하나로 합치기 시설, 취소규정, 사진 완료 test중

--- src/main/java/com/example/stay/accommodation/hotelStory/mapper/HotelStoryMapper.java
+++ src/main/java/com/example/stay/accommodation/hotelStory/mapper/HotelStoryMapper.java
@@ -14,4 +14,7 @@ public interface HotelStoryMapper {
     String insertRoomType(String strRoomTypeName, int intAID, int intMinPersons, int intMaxPersons, String strRoomTypeId, int intStep, String strIngYn, String strText1, String strText2);
 
     String insertRatePlan(int intAID, String strAccommId, int intToconIdx, String strRoomTypeId, String strRatePlanId, String strRatePlanName, String strBedTypeCode, String strMealCode, int intMinPersons, int intMaxPersons, int intPrice);
+
+    String insertAccommtotal(String strPropertyId, String strPropertyName, String strAddress, String strPhone, String strNumRooms, String strLocation, String strHomePageUrl, String strCheckInTime, String strCheckOutTime
+            , String strLongitude, String strLatitude, String strCity, String strPropertyDescription, String strTrafficInformation, String strRoomInformation, String imgData, String cancelData);
 }
