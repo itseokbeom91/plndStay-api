bd165b166bc5c68b3f95558ee084700c64a01169
yoojung <syjung618@naver.com>
2023-06-14 10:59:22 +0900

    온다 시설, 객실 정리 작업

--- src/main/java/com/example/stay/accommodation/onda/mapper/AccomodationMapper.java
+++ src/main/java/com/example/stay/accommodation/onda/mapper/AccomodationMapper.java
@@ -9,15 +9,15 @@ import java.util.Map;
 public interface AccomodationMapper {
 
     String insertAccommTotal(String strPropertyID, String strDeleteYn, String strViewYn, String strType,
-                             String intDistrict1, String intDistrict2, String intDistrict3, String strSubject, String strLat, String strLon,
+                             int intDistrict1, int intDistrict2, String strSubject, String strLat, String strLon,
                              String strCheckIn, String strCheckOut, String strPhone, String strFax, String strEmail, String strZipCode,
                              String strAddr1, String strAddr2, String strDescription, String strRsvGuide, String strAcmNotice,String strImgDatas,
                              String strPenaltyDatas, String strKeyWordDatas, String strFacilityDatas, String strRmtypeDatas);
 
     String updateRmtype(String strPropertyID, String strType, String strRmtypeDatas);
 
-    String updateGoods(int strRatePlanId, int intStock, String strCheckInDate, int intBasicPrice, int intSalePrice,
-                       int intMinStay, int intMaxStay);
+    String updateGoods(String strRateplanID, String strRmtypeID, String strDateSales, int intStock, int intCost, int intSales,
+                       int intExtraA, int intExtraC, int intExtraB, int intOmkStock, double intOmkSales);
 
     int getRoomAdminCnt(String intCondoId);
 
@@ -25,8 +25,7 @@ public interface AccomodationMapper {
 
     String updateStatus(String target, String strDeleteYn, String strViewYn, String propertyId, String roomTypeId, String ratePlanId);
 
-    String getDistrictCodeByStrName(String strDistrict);
-    String getDistrictCodeByStrName2(String strDistrict);
+    Map<String, Integer> getDistrictCode(String strRegion, String strCity);
 
     String getStrCodeByStrName(String strCateCode, String strName);
 
