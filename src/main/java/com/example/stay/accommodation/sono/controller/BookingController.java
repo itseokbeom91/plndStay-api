84082d50f8b2df8d0013533e747ac468eaf1d02e
woonbeom.lee <bumy484@gmail.com>
2023-06-22 09:02:54 +0900

    소노 + 리솜 객실 + 재고 DB INSERT ~

--- src/main/java/com/example/stay/accommodation/sono/controller/BookingController.java
+++ src/main/java/com/example/stay/accommodation/sono/controller/BookingController.java
@@ -1,14 +1,9 @@
 package com.example.stay.accommodation.sono.controller;
 
 import com.example.stay.accommodation.sono.service.BookingService;
-import com.example.stay.common.util.ResponseResult;
-import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,60 +14,70 @@ public class BookingController {
     @Autowired
     BookingService bookingService = new BookingService();
 
+
     @GetMapping("/getPackageList")
     @ResponseBody
-    public ResponseResult getPackageList(HttpServletRequest httpServletRequest){
-        System.out.println("패키지 목록 조회");
-        ResponseResult responseResult = bookingService.getPackageList(httpServletRequest);
-        return  responseResult;
+    public String getPackageList(HttpServletRequest httpServletRequest){
+        return  bookingService.getPackageList(httpServletRequest);
     }
 
     @GetMapping("/getPackageInfo")
     @ResponseBody
-    public ResponseResult getPackageInfo(String pkgNo, HttpServletRequest httpServletRequest){
-        System.out.println("패키지 목록 조회");
-        ResponseResult responseResult = bookingService.getPackageInfo(pkgNo, httpServletRequest);
-        return  responseResult;
+    public String getPackageInfo(String pkgNo, HttpServletRequest httpServletRequest){
+        return  bookingService.getPackageInfo(pkgNo, httpServletRequest);
     }
 
     @GetMapping("/getPackageStatus")
     @ResponseBody
-    public ResponseResult getPackageStatus(String pkgNo, String storeCd, String sDate, String rmTypeCd, String ciYmd, HttpServletRequest httpServletRequest){
-        System.out.println("패키지 목록 조회");
-        ResponseResult responseResult = bookingService.getPackageStatus(pkgNo, storeCd, sDate, rmTypeCd, ciYmd, httpServletRequest);
-        return  responseResult;
+    public String getPackageStatus(String pkgNo, String storeCd, String sDate, String rmTypeCd, String ciYmd, HttpServletRequest httpServletRequest){
+        return  bookingService.getPackageStatus(pkgNo, storeCd, sDate, rmTypeCd, ciYmd, httpServletRequest);
     }
 
     @GetMapping("/getPackageAmount")
     @ResponseBody
-    public ResponseResult getPackageAmount(String pkgNo, String storeCd, String sDate, String rmTypeCd, String ciYmd, String nights, String rmCnt, HttpServletRequest httpServletRequest){
-        System.out.println("패키지 목록 조회");
-        ResponseResult responseResult = bookingService.getPackageAmount(pkgNo, storeCd, sDate, rmTypeCd, ciYmd, nights, rmCnt, httpServletRequest);
-        return  responseResult;
+    public String getPackageAmount(String pkgNo, String storeCd, String sDate, String rmTypeCd, String ciYmd, String nights, String rmCnt, HttpServletRequest httpServletRequest){
+        return  bookingService.getPackageAmount(pkgNo, storeCd, sDate, rmTypeCd, ciYmd, nights, rmCnt, httpServletRequest);
     }
 
     @GetMapping("/reservation")
     @ResponseBody
-    public ResponseResult reservation(String pkgNo, String storeCd, String ciYmd, String rmTypeCd, String comRsvNo, String userName, String userTel, String payAmt, String adultCnt, String childCnt ,HttpServletRequest httpServletRequest){
-        System.out.println("패키지 목록 조회");
-        ResponseResult responseResult = bookingService.reservation(pkgNo, storeCd, ciYmd, rmTypeCd, comRsvNo, userName, userTel, payAmt, adultCnt, childCnt ,httpServletRequest);
-        return  responseResult;
+    public String reservation(String pkgNo, String storeCd, String ciYmd, String rmTypeCd, String comRsvNo, String userName, String userTel, String payAmt, String adultCnt, String childCnt ,HttpServletRequest httpServletRequest){
+        return  bookingService.reservation(pkgNo, storeCd, ciYmd, rmTypeCd, comRsvNo, userName, userTel, payAmt, adultCnt, childCnt ,httpServletRequest);
     }
 
     @GetMapping("/getRoomList")
     @ResponseBody
-    public ResponseResult getRoomList(HttpServletRequest httpServletRequest){
-        System.out.println("패키지 목록 조회");
-        ResponseResult responseResult = bookingService.getRoomList(httpServletRequest);
-        return  responseResult;
+    public String getRoomList(HttpServletRequest httpServletRequest){
+        return  bookingService.getRoomList(httpServletRequest);
+    }
+
+    @GetMapping("/getRoomAmount")
+    @ResponseBody
+    @CrossOrigin
+    public String getRoomAmount(HttpServletRequest httpServletRequest, String storeCd, String sMonth){
+        return  bookingService.getRoomAmount(httpServletRequest, storeCd, sMonth);
+    }
+
+
+
+    @GetMapping("/getRoomStatus")
+    @ResponseBody
+    public String getRoomStatus(HttpServletRequest httpServletRequest, String storeCd, String sDate){
+        return  bookingService.getRoomStatus(httpServletRequest, storeCd, sDate);
     }
 
     @GetMapping("/insertSONO")
     @ResponseBody
     public String insertSONO(HttpServletRequest httpServletRequest) {
-        String result = bookingService.insertSONO(httpServletRequest);
-        return result;
+        return bookingService.insertSONO(httpServletRequest);
     }
+
+    @GetMapping("/stockResult")
+    @ResponseBody
+    public String getStockAndInsert(HttpServletRequest httpServletRequest) {
+        return bookingService.getStockAndInsert(httpServletRequest);
+    }
+
 
 
 }
--- src/main/java/com/example/stay/accommodation/sono/mapper/BookingMapper.java
+++ src/main/java/com/example/stay/accommodation/sono/mapper/BookingMapper.java
@@ -2,9 +2,16 @@ package com.example.stay.accommodation.sono.mapper;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
+
 
 @Repository("sono.BookingMapper")
 public interface BookingMapper {
     String insertRoom(String strPackageDatas, String strRoomDatas, String strStockDatas, String strAccommDatas, String strType);
+
+    String updatePackageStock(String strPackageStockDatas);
+
+    List<Map<String, Object>> getPackageCodeAndStoreCode(String type);
 
 }
