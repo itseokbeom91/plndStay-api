8133bc78d5a9b92ad59f2915e691fe5ce4898b86
woonbeom.lee <bumy484@gmail.com>
2023-06-23 16:31:31 +0900

    같은 지역임에도 지역명을 다르게 표기하는 경우가 많아 지역코드로 선매칭 처리
    ex) 강원, 강원도, 강원특별자치도 => 42

--- src/main/java/com/example/stay/common/util/CommonFunction.java
+++ src/main/java/com/example/stay/common/util/CommonFunction.java
@@ -46,41 +46,41 @@ public class CommonFunction<T> {
     ex) 강원, 강원특별자치도, 강원도 => 42
      */
     public String addressToDistrictCode(String address) {
-        String result = "";
         if ("서울".equals(address) || "서울시".equals(address) || "서울특별시".equals(address)) {
-            result = "11";
+            return "11";
         } else if ( "부산".equals(address) ||  "부산시".equals(address) || "부산광역시".equals(address)) {
-            result = "26";
+            return "26";
         } else if ( "대구".equals(address) ||  "대구시".equals(address) || "대구광역시".equals(address)) {
-            result = "27";
+            return "27";
         } else if ( "인천".equals(address) ||  "인천시".equals(address) || "인천광역시".equals(address)) {
-            result = "28";
+            return "28";
         } else if ( "광주".equals(address) ||  "광주시".equals(address) || "광주광역시".equals(address)) {
-            result = "29";
+            return "29";
         } else if ( "대전".equals(address) ||  "대전시".equals(address) || "대전광역시".equals(address)) {
-            result = "30";
+            return "30";
         } else if ( "울산".equals(address) ||  "울산시".equals(address) || "울산광역시".equals(address)) {
-            result = "31";
+            return "31";
         } else if ( "경기".equals(address) ||  "경기도".equals(address)) {
-            result = "41";
+            return "41";
         } else if ( "강원".equals(address) ||  "강원도".equals(address) || "강원특별자치도".equals(address)) {
-            result = "42";
+            return "42";
         } else if ( "충북".equals(address) ||  "충청북도".equals(address)) {
-            result = "43";
+            return "43";
         } else if ( "충남".equals(address) ||  "충청남도".equals(address)) {
-            result = "44";
+            return "44";
         } else if ( "전북".equals(address) ||  "전라북도".equals(address)) {
-            result = "45";
+            return "45";
         } else if ( "전남".equals(address) ||  "전라남도".equals(address)) {
-            result = "46";
+            return "46";
         } else if ( "경북".equals(address) ||  "경상북도".equals(address)) {
-            result = "47";
+            return "47";
         } else if ( "경남".equals(address) ||  "경상남도".equals(address)) {
-            result = "48";
+            return "48";
         } else if ( "제주".equals(address) ||  "제주도".equals(address) || "제주특별자치도".equals(address)) {
-            result = "50";
+            return "50";
+        } else {
+            return "";
         }
 
-        return result;
     }
 }
