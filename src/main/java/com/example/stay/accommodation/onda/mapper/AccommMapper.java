0933e20a0ab5b5d99c3b5591a48e012b539b14d9
yoojung <syjung618@naver.com>
2023-07-14 17:16:22 +0900

    strType 변경 및 strApiFlag 컬럼 추가에 대한 수정

--- src/main/java/com/example/stay/accommodation/onda/mapper/AccommMapper.java
+++ src/main/java/com/example/stay/accommodation/onda/mapper/AccommMapper.java
@@ -8,13 +8,13 @@ import java.util.Map;
 @Repository("onda.AccommMapper")
 public interface AccommMapper {
 
-    String insertAccommTotal(String strPropertyID, String strDeleteYn, String strViewYn, String strType,
+    String insertAccommTotal(String strPropertyID, String strDeleteYn, String strViewYn, String strApiFlag,
                              String strDistrict1, String strDistrict2, String strSubject, String strLat, String strLon,
                              String strCheckIn, String strCheckOut, String strPhone, String strFax, String strEmail, String strZipCode,
                              String strAddr1, String strAddr2, String strDescription, String strRsvGuide, String strAcmNotice,String strImgDatas,
                              String strPenaltyDatas, String strKeyWordDatas, String strAttractionDatas, String strFacilityDatas, String strRmtypeDatas);
 
-    String updateRmtype(String strPropertyID, String strType, String strRmtypeDatas);
+    String updateRmtype(String strPropertyID, String strRmtypeDatas);
 
     String updateGoods(int intAID, int intRmIdx, String strStockDatas);
 
