03bc272c293a052c2ea3491356ddba3e4bc1608d
yoojung <syjung618@naver.com>
2023-09-05 17:48:09 +0900

    엘리시안 api 수정

--- src/main/java/com/example/stay/accommodation/elysian_gangchon/mapper/ElysianMapper.java
+++ src/main/java/com/example/stay/accommodation/elysian_gangchon/mapper/ElysianMapper.java
@@ -1,10 +1,21 @@
 package com.example.stay.accommodation.elysian_gangchon.mapper;
 
+import com.example.stay.openMarket.common.dto.RsvStayDto;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository
 public interface ElysianMapper {
     String updateGoods(int intAID, int intRmIdx, String strStockDatas);
 
     int getIntAID(int intRmIdx);
+
+    Map<String, String> getPackage(int intRmIdx);
+
+    RsvStayDto getReservation(int intRsvID);
+
+    String updateRsvStay(int intRsvID, String strStatusCode, String strRsvRmNum);
+
+    String getStrRsvRmNum(int intRsvID);
 }
