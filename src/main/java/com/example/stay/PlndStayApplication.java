87c4779acc28feead1af9dab0a65c6e5b8faf335
woonbeom.lee <bumy484@gmail.com>
2023-10-27 13:41:30 +0900

    비동기처리위한 어노테이션 추가

--- src/main/java/com/example/stay/PlndStayApplication.java
+++ src/main/java/com/example/stay/PlndStayApplication.java
@@ -3,10 +3,12 @@ package com.example.stay;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan(value = {"com.example.stay.*.*.mapper", "com.example.stay.*.mapper"})
 @EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class PlndStayApplication {
 
